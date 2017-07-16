32026edb1cbb8299e364365bf22d1499c93e7693
Cloud <cloud@txthinking.com>
2017-07-16 21:42:26 +0800

    build for ARM

--- cli/brook/buildAll.sh
+++ cli/brook/buildAll.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 GOOS=linux GOARCH=amd64 go build -o brook .
-GOOS=linux GOARCH=386 go build -o brook386 .
-GOOS=darwin GOARCH=amd64 go build -o brookmacos .
-GOOS=windows GOARCH=amd64 go build -o brookserver.exe .
-GOOS=windows GOARCH=386 go build -o brookserver.386.exe .
+GOOS=linux GOARCH=386 go build -o brook_linux_386 .
+GOOS=linux GOARCH=arm64 go build -o brook_linux_arm64 .
+GOOS=linux GOARCH=arm GOARM=7 go build -o brook_linux_arm_7 .
+GOOS=linux GOARCH=arm GOARM=6 go build -o brook_linux_arm_6 .
+GOOS=linux GOARCH=arm GOARM=5 go build -o brook_linux_arm_5 .
+GOOS=darwin GOARCH=amd64 go build -o brook_macos_amd64 .
+GOOS=windows GOARCH=amd64 go build -o brook_windows_amd64.exe .
+GOOS=windows GOARCH=386 go build -o brook_windows_386.exe .
---
+++ whitehttpmiddleman.go
@@ -0,0 +1,69 @@
+package brook
+
+import (
+	"io"
+	"net"
+	"time"
+
+	"github.com/txthinking/pac/blackwhite"
+)
+
+// WhiteHTTPMiddleman is a HTTPMiddleman who only handle domain in white list
+type WhiteHTTPMiddleman struct {
+	Timeout  int
+	Deadline int
+}
+
+// NewWhiteHTTPMiddleman returns a WhiteHTTPMiddleman which can used to handle http proxy request
+func NewWhiteHTTPMiddleman(timeout, deadline int) *WhiteHTTPMiddleman {
+	blackwhite.InitWhiteList()
+	return &WhiteHTTPMiddleman{
+		Timeout:  timeout,
+		Deadline: deadline,
+	}
+}
+
+// HandleHTTPProxy handle http proxy request, if the domain is in the white list
+func (w *WhiteHTTPMiddleman) HandleHTTPProxy(method, addr string, request []byte, conn *net.TCPConn) (handled bool, err error) {
+	host, _, err := net.SplitHostPort(addr)
+	if err != nil {
+		return false, err
+	}
+	if !blackwhite.IsWhite(host) {
+		return false, nil
+	}
+
+	rc, err := net.Dial("tcp", addr)
+	if err != nil {
+		return true, err
+	}
+	defer rc.Close()
+	if w.Timeout != 0 {
+		if rtc, ok := rc.(*net.TCPConn); ok {
+			if err := rtc.SetKeepAlivePeriod(time.Duration(w.Timeout) * time.Second); err != nil {
+				return true, err
+			}
+		}
+	}
+	if w.Deadline != 0 {
+		if err := rc.SetDeadline(time.Now().Add(time.Duration(w.Deadline) * time.Second)); err != nil {
+			return true, err
+		}
+	}
+	if method == "CONNECT" {
+		_, err := conn.Write([]byte("HTTP/1.1 200 Connection established\r\n\r\n"))
+		if err != nil {
+			return true, err
+		}
+	}
+	if method != "CONNECT" {
+		if _, err := rc.Write(request); err != nil {
+			return true, err
+		}
+	}
+	go func() {
+		_, _ = io.Copy(rc, conn)
+	}()
+	_, _ = io.Copy(conn, rc)
+	return true, nil
+}
