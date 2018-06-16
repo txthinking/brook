8dc244468b8f67c2f9461914ac09ff16ab97105f
_eternal_flame <eternal_flame-AD@protonmail.com>
2018-06-16 21:39:12 +0800

    use windows api

--- sysproxy/system_windows.go
+++ sysproxy/system_windows.go
@@ -3,11 +3,8 @@ package sysproxy
 import (
 	"bytes"
 	"errors"
-	"fmt"
-	"os"
 	"os/exec"
 	"syscall"
-	"time"
 
 	"github.com/txthinking/ant"
 )
@@ -32,7 +29,7 @@ func TurnOnSystemProxy(pac string) error {
 		}
 		return errors.New("reg add pe:" + string(u) + ":" + err.Error())
 	}
-	if err := restartWindowsIE(); err != nil {
+	if err := reloadWinProxy(); err != nil {
 		return err
 	}
 	return nil
@@ -65,52 +62,28 @@ func TurnOffSystemProxy() error {
 			}
 		}
 	}
-	if err := restartWindowsIE(); err != nil {
+	if err := reloadWinProxy(); err != nil {
 		return err
 	}
 	return nil
 }
 
-func restartWindowsIE() error {
-	c := exec.Command(`tasklist`, `/fo`, `list`, `/fi`, `imagename eq iexplore.exe`)
-	c.SysProcAttr = &syscall.SysProcAttr{HideWindow: true}
-	if out, err := c.CombinedOutput(); err != nil {
-		u, e := ant.GBK2UTF8(out)
-		if e != nil {
-			return errors.New("tasklist ie g2u:" + e.Error())
-		}
-		return errors.New("tasklist ie:" + string(u) + ":" + err.Error())
-	} else {
-		u, e := ant.GBK2UTF8(out)
-		if e != nil {
-			return errors.New("tasklist ie g2u:" + e.Error())
-		}
-		if bytes.Contains(bytes.ToLower(u), []byte("iexplore.exe")) {
-			c := exec.Command(`taskkill`, `/f`, `/t`, `/im`, `iexplore.exe`)
-			c.SysProcAttr = &syscall.SysProcAttr{HideWindow: true}
-			if out, err := c.CombinedOutput(); err != nil {
-				u, e := ant.GBK2UTF8(out)
-				if e != nil {
-					return errors.New("taskkill ie before start g2u:" + e.Error())
-				}
-				return errors.New("taskkill ie before start:" + string(u) + ":" + err.Error())
-			}
-		}
+func reloadWinProxy() error {
+	h, err := syscall.LoadLibrary("wininet.dll")
+	if err != nil {
+		return err
 	}
-	c = exec.Command(fmt.Sprintf(`%s\Internet Explorer\iexplore.exe`, os.Getenv("PROGRAMFILES")), `-nohome`)
-	c.SysProcAttr = &syscall.SysProcAttr{HideWindow: true}
-	if err := c.Start(); err != nil {
-		return errors.New("ie start:" + err.Error())
+	f, err := syscall.GetProcAddress(h, "InternetSetOptionW")
+	if err != nil {
+		return err
 	}
-	time.Sleep(2 * time.Second)
-	c = exec.Command(`taskkill`, `/f`, `/t`, `/im`, `iexplore.exe`)
-	c.SysProcAttr = &syscall.SysProcAttr{HideWindow: true}
-	if out, err := c.CombinedOutput(); err != nil {
-		u, e := ant.GBK2UTF8(out)
-		if e != nil {
-			return errors.New("taskkill ie after start g2u:" + e.Error())
-		}
-		return errors.New("taskkill ie after start:" + string(u) + ":" + err.Error())
+	ret, _, errno := syscall.Syscall6(uintptr(f), 4, 0, 39, 0, 0, 0, 0)
+	if ret != 1 {
+		return errors.New(errno.Error())
+	}
+	ret, _, errno = syscall.Syscall6(uintptr(f), 4, 0, 37, 0, 0, 0, 0)
+	if ret != 1 {
+		return errors.New(errno.Error())
 	}
 	return nil
 }
